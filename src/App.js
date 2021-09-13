cfc76e6a1771110ecaeea60d0cfe528e61f22c55
Eslam Akram <eslam_akram91@yahoo.com>
2021-09-13 15:35:43 +0300

    solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NavBAR from './Components/NavBAR';
+// import NavBAR from './Components/NavBAR';
 import ExploreForm from './Components/ExploreForm';
 import LocationIQ from './Components/LocationIQ';
 import axios from 'axios';
@@ -17,9 +17,11 @@ import axios from 'axios';
         mapSrc:''
       };
     }
-      handleLocation = (event) => {
+    
+      handleLocation = async (event) => {
         let selectedCity = event.target.value;
-        this.setState = ({
+        console.log(selectedCity, 'hi');
+        await this.setState({
           display_name: selectedCity
         })
       }
@@ -27,49 +29,59 @@ import axios from 'axios';
 
       handleSubmit = (e) => {
         e.preventDefault();
-        let config = {
-          method: 'GET',
-          baseURL: `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}&format=json`
+        console.log('i sended data')
 
+        let config = {
+                    method: 'GET',
+          // baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}&format=json`
+          baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
         }
+        console.log(config)
+        console.log(this.state)
       
 // GET request for location info 
       axios(config).then(response => {
         let responseData = response.data[0];
-        this.setState = ({
+        this.setState({
           showData: true,
           display_name: responseData.display_name,
           type: responseData.type,
-          latitude: responseData.latitude,
-          longitude: responseData.longitude,
+          latitude: responseData.lat,
+          longitude: responseData.lon,
           mapSrc:responseData.imgSrc
         })
       })
 
 
-// GET request for map image 
-axios({
-  method: 'GET',
-  url: `https://tiles.locationiq.com/v3/hybrid/raster?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&format=json&q=${this.state.display_name}`,
-})
-  .then(response => {
-   let mapResponse= response.data[0];
-    this.setState =({
-      mapSrc: mapResponse.url
-    })
-  });
+//GET request for map image 
+// axios({
+//   method: 'GET',
+//   url: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18&q=${this.state.display_name}`,
+// })
+//   .then(response => {
+//    let mapResponse= response.data[0];
+//     this.setState({
+//       mapSrc: mapResponse.url
+//     })
+//   });
 
     }
     render() {
     return (
       <>
-        <NavBAR />
-        <ExploreForm handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
-        {this.state.showData && <LocationIQ   display_name={this.state.display_name}
-                      type= {this.state.type}
-                      latitude= {this.state.latitude}
-                      longitude= { this.state.longitude} />}
-
+        {/* <NavBAR /> */}
+        <ExploreForm 
+                     handleLocation={this.handleLocation} 
+                     handleSubmit={this.handleSubmit} 
+                    />
+         {this.state.showData && 
+          <LocationIQ    
+                       display_name = {this.state.display_name}
+                       type = {this.state.type}
+                       latitude = {this.state.latitude}
+                       longitude = { this.state.longitude} 
+                        />  
+ }
 
       </>
     )
