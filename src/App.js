60a761d7eb7114bce97a6d8916b9cc63d492b079
Eslam Akram <eslam_akram91@yahoo.com>
2021-09-14 07:47:46 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -2,88 +2,97 @@ import React, { Component } from 'react';
 // import NavBAR from './Components/NavBAR';
 import ExploreForm from './Components/ExploreForm';
 import LocationIQ from './Components/LocationIQ';
+import Weather from './Components/Weather';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
- class App extends Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        showData: false,
-        display_name: " ",
-        type: " ",
-        latitude: " ",
-        longitude: " ",
-        mapSrc:''
-      };
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showData: false,
+      display_name: " ",
+      type: " ",
+      latitude: " ",
+      longitude: " ",
+      mapSrc: '',
+      weatherData: [],
+    };
+  }
+
+  handleLocation = async (event) => {
+    let selectedCity = event.target.value;
+    console.log(selectedCity, 'hi');
+    await this.setState({
+      display_name: selectedCity
+    })
+  }
+
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('i sended data')
+
+    let config = {
+      method: 'GET',
+      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
     }
-    
-      handleLocation = async (event) => {
-        let selectedCity = event.target.value;
-        console.log(selectedCity, 'hi');
-        await this.setState({
-          display_name: selectedCity
-        })
-      }
+    console.log(config)
+    console.log(this.state)
 
-
-      handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('i sended data')
-
-        let config = {
-                    method: 'GET',
-          // baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}&format=json`
-          baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
-        }
-        console.log(config)
-        console.log(this.state)
-      
-// GET request for location info 
-      axios(config).then(response => {
-        let responseData = response.data[0];
-        this.setState({
-          showData: true,
-          display_name: responseData.display_name,
-          type: responseData.type,
-          latitude: responseData.lat,
-          longitude: responseData.lon,
-          mapSrc:responseData.imgSrc
-        })
+    // GET request for location info 
+    axios(config).then(response => {
+      let responseData = response.data[0];
+      this.setState({
+        showData: true,
+        display_name: responseData.display_name,
+        type: responseData.type,
+        latitude: responseData.lat,
+        longitude: responseData.lon,
+        mapSrc: responseData.imgSrc
       })
+    }).then(() => {
+      axios.get(`http://${process.env.REACT_APP_BACKEND_URL}/weather?lat=${this.state.latitude}&lon=${this.state.longitude}`).then(res => {
+        console.log(res.data)
+        this.setState({
+          weatherData: res.data
+        });
+      });
+    });
 
 
-//GET request for map image 
-// axios({
-//   method: 'GET',
-//   url: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&zoom=1-18&q=${this.state.display_name}`,
-// })
-//   .then(response => {
-//    let mapResponse= response.data[0];
-//     this.setState({
-//       mapSrc: mapResponse.url
-//     })
-//   });
 
-    }
-    render() {
+
+  }
+  render() {
     return (
       <>
         {/* <NavBAR /> */}
-        <ExploreForm 
-                     handleLocation={this.handleLocation} 
-                     handleSubmit={this.handleSubmit} 
-                    />
-         {this.state.showData && 
-          <LocationIQ    
-                       display_name = {this.state.display_name}
-                       type = {this.state.type}
-                       latitude = {this.state.latitude}
-                       longitude = { this.state.longitude} 
-                        />  
- }
+        <ExploreForm
+          handleLocation={this.handleLocation}
+          handleSubmit={this.handleSubmit}
+        />
+        {this.state.showData &&
+          <LocationIQ
+            display_name={this.state.display_name}
+            type={this.state.type}
+            latitude={this.state.latitude}
+            longitude={this.state.longitude}
+          />
+        }
+
+        {
+          this.state.weatherData.map( day=> {
+           return  <Weather  
+                       date={day.date}
+                       description={day.description}
+              // display_name={this.state.display_name.split(',')}
+            />
+
+          })
+        } 
 
       </>
     )
---
+++ src/Components/Weather.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Weather extends Component {
+    render() {
+        return (
+            <Card border="success" style={{ width: '20rem', margin:'30px' , color:'gray'}}>
+                <Card.Header>Date: {this.props.date}</Card.Header>
+                <Card.Body>
+                    <Card.Title> <img src="https://www.indivstock.com/static27/preview2/stock-vector-sunny-cloud-daylight-weather-forecast-icon-symbol-design-391262.jpg" style={{ width: '5rem'}}></img> </Card.Title>
+                    <Card.Text>
+                      Description: {this.props.description}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
+
+export default Weather
